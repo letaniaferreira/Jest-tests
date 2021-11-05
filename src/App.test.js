7419e378dd59139f25e84455e83bd6d27fcb0ba6
Letania Ferreira <letania@rollbar.com>
2021-11-05 15:08:18 -0700

    Test by role

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// finds element with role of button and correct text
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButtton = screen.getByRole('button', { name: 'Change to blue'});
+  expect(colorButtton).toHaveStyle({ backgroundColor: 'red'})
+});
+// finds element with role of button and correct text
+test('button turns blue when clicked', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor:'blue' });
 });
